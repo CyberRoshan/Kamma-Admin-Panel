729263450a293a436039849a92f025918267d978
CyberRoshan <roshanchaurasia990@gmail.com>
2024-09-06 09:37:24 +0530

    work in progress

--- src/script.js
+++ src/script.js
@@ -1,98 +1,84 @@
-// function toggleDropdown() {
-//     const dropdown = document.getElementById("dropdown");
-//     dropdown.classList.toggle("hidden");
-// }
-
-// function toggleSelectBox() {
-//     const dropdown = document.getElementById("SearchPostcode");
-//     selectBox.classList.toggle("hidden");
-// }
-
 // ! index.html
-$("#dropdownButton").click(()=>{
-    $("#dropdown").toggleClass("hidden")
-})
+$("#dropdownButton").click(() => {
+    $("#dropdown").toggleClass("hidden");
+});
 
-$(".dropDownItems").click(function() {
+$(".dropDownItems").click(function () {
     let data = $(this).html();
     console.log(data);
-    $("#dropdownButton").html(data)
-    $("#dropdown").fadeOut(200)
-    $("#dropdownButton").click(()=>{
-        $("#dropdown").fadeIn(200)
-    })
+    $("#dropdownButton").html(data);
+    $("#dropdown").fadeOut(200);
+    $("#dropdownButton").click(() => {
+        $("#dropdown").fadeIn(200);
+    });
 });
 // ! index.html
 
-
 // ! adjust.html
-
 
 // ! Type of Property Detail Section Start
 
-$("#adjustTabbingBox").addClass("md:rounded-lg")
-$("#navGreyLine").removeClass("lg:block hidden border-b-2 b border-grey-2 w-full absolute top-[75%] left-0")
-$("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-$("#TypePropertyDetailSection").fadeOut()
-$("#AgePropertyDetailSection").hide()
-$("#FloorAreaDetailSection").hide()
-$("#p1").removeClass("hidden")
-$("#svg1").removeClass("lg:hidden")
+$("#adjustTabbingBox").addClass("md:rounded-lg");
+$("#navGreyLine").removeClass("lg:block hidden border-b-2 b border-grey-2 w-full absolute top-[75%] left-0");
+$("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+$("#TypePropertyDetailSection").fadeOut();
+$("#AgePropertyDetailSection").hide();
+$("#FloorAreaDetailSection").hide();
+$("#p1").removeClass("hidden");
+$("#svg1").removeClass("lg:hidden");
 
-$("#typeOfProperty").click(()=>{
-    $("#TypePropertyDetailSection").fadeIn()
-    $("#AgePropertyDetailSection").hide()
-    $("#FloorAreaDetailSection").hide()
-    $("#adjustTabbingBox").removeClass("md:rounded-lg")
-    $("#navGreyLine").addClass("lg:block hidden border-b-2 b border-grey-2 w-full absolute top-[75%] left-0")
-    $("#typeOfProperty").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#ageOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#FloorArea").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#p1").addClass("hidden")
-    $("#svg1").addClass("lg:hidden")
-    $("#p2").removeClass("hidden")
-    $("#svg2").removeClass("lg:hidden")
-    $("#p3").removeClass("hidden")
-    $("#svg3").removeClass("lg:hidden")
-})
-
+$("#typeOfProperty").click(() => {
+    $("#TypePropertyDetailSection").fadeIn();
+    $("#AgePropertyDetailSection").hide();
+    $("#FloorAreaDetailSection").hide();
+    $("#adjustTabbingBox").removeClass("md:rounded-lg");
+    $("#navGreyLine").addClass("lg:block hidden border-b-2 b border-grey-2 w-full absolute top-[75%] left-0");
+    $("#typeOfProperty").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#ageOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#FloorArea").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#p1").addClass("hidden");
+    $("#svg1").addClass("lg:hidden");
+    $("#p2").removeClass("hidden");
+    $("#svg2").removeClass("lg:hidden");
+    $("#p3").removeClass("hidden");
+    $("#svg3").removeClass("lg:hidden");
+});
 
 // $(".propertySelection").click(()=>{
 //     $(".propertySelection")
 // })
 // ! Type of Property Detail Section End
 // ! Age of Property Detail Section Start
-$("#ageOfProperty").click(()=>{
-    $("#ageOfProperty").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#FloorArea").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#TypePropertyDetailSection").hide()
-    $("#FloorAreaDetailSection").hide()
-    $("#AgePropertyDetailSection").fadeIn()
-    $("#p2").addClass("hidden")
-    $("#svg2").addClass("lg:hidden")
-    $("#p1").removeClass("hidden")
-    $("#svg1").removeClass("lg:hidden")
-    $("#p3").removeClass("hidden")
-    $("#svg3").removeClass("lg:hidden")
-})
+$("#ageOfProperty").click(() => {
+    $("#ageOfProperty").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#FloorArea").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#TypePropertyDetailSection").hide();
+    $("#FloorAreaDetailSection").hide();
+    $("#AgePropertyDetailSection").fadeIn();
+    $("#p2").addClass("hidden");
+    $("#svg2").addClass("lg:hidden");
+    $("#p1").removeClass("hidden");
+    $("#svg1").removeClass("lg:hidden");
+    $("#p3").removeClass("hidden");
+    $("#svg3").removeClass("lg:hidden");
+});
 // ! Age of Property Detail Section End
 // ! Floor Area Detail Section Start
-$("#FloorArea").click(()=>{
-    $("#FloorArea").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#ageOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5")
-    $("#TypePropertyDetailSection").hide()
-    $("#AgePropertyDetailSection").hide()
-    $("#FloorAreaDetailSection").fadeIn()
-    $("#p2").removeClass("hidden")
-    $("#svg2").removeClass("lg:hidden")
-    $("#p1").removeClass("hidden")
-    $("#svg1").removeClass("lg:hidden")
-    $("#p3").addClass("hidden")
-    $("#svg3").addClass("lg:hidden")
-
-})
+$("#FloorArea").click(() => {
+    $("#FloorArea").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#ageOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
+    $("#TypePropertyDetailSection").hide();
+    $("#AgePropertyDetailSection").hide();
+    $("#FloorAreaDetailSection").fadeIn();
+    $("#p2").removeClass("hidden");
+    $("#svg2").removeClass("lg:hidden");
+    $("#p1").removeClass("hidden");
+    $("#svg1").removeClass("lg:hidden");
+    $("#p3").addClass("hidden");
+    $("#svg3").addClass("lg:hidden");
+});
 // ! Floor Area Detail Section End
 
 // $("#adjustTabbingBox #typeOfProperty")
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'Nunito': ['"Nunito Sans"','"sans-serif"']
+        Nunito: ['"Nunito Sans"', '"sans-serif"'],
       },
       colors: {
-        'coral' : '#FF4D72',
-        'theme-black': '#20262C',
-        'grey-1': '#8F948F',
-        'grey-2': '#D9DED9',
-        'grey-3': '#6C726C',
-        'grey-4': '#C1C1C1',
-        'grey-5': '#EDF0ED',
-        'back-grey':'#F4F6F8',
-        'turquoise': '#19D3C5',
-        'Midnight_Blue': '#34485C',
-        'yellow': '#FFC844',
-
-
+        coral: "#FF4D72",
+        "theme-black": "#20262C",
+        "grey-1": "#8F948F",
+        "grey-2": "#D9DED9",
+        "grey-3": "#6C726C",
+        "grey-4": "#C1C1C1",
+        "grey-5": "#EDF0ED",
+        "back-grey": "#F4F6F8",
+        turquoise: "#19D3C5",
+        Midnight_Blue: "#34485C",
+        yellow: "#FFC844",
       },
       boxShadow: {
-        'custom-orange': '0 4px 6px -1px rgba(243, 161, 83, 0.13)',
-        'custom-orange-17': '0 4px 6px -1px rgba(243, 161, 83, 0.17)',
+        "custom-orange": "0 4px 6px -1px rgba(243, 161, 83, 0.13)",
+        "custom-orange-17": "0 4px 6px -1px rgba(243, 161, 83, 0.17)",
       },
     },
   },
   plugins: [],
-}
+};
