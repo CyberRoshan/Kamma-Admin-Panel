fdccfdb3989c4aa41c8bdc30ea07546ca158024f
CyberRoshan <roshanchaurasia990@gmail.com>
2024-09-08 17:21:03 +0530

    Code added to codebase

--- src/script.js
+++ src/script.js
@@ -16,71 +16,35 @@ $(".dropDownItems").click(function () {
 
 // ! adjust.html Start
 
-// ! Type of Property Detail Section Start
-
-$("#adjustTabbingBox").addClass("md:rounded-lg");
-$("#navGreyLine").removeClass("lg:block hidden border-b-2 b border-grey-2 w-full absolute top-[75%] left-0");
-$("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-$("#TypePropertyDetailSection").fadeOut();
-$("#AgePropertyDetailSection").hide();
-$("#FloorAreaDetailSection").hide();
-$("#p1").removeClass("hidden");
-$("#svg1").removeClass("lg:hidden");
-
-$("#typeOfProperty").click(() => {
-    $("#TypePropertyDetailSection").fadeIn();
-    $("#AgePropertyDetailSection").hide();
-    $("#FloorAreaDetailSection").hide();
-    $("#adjustTabbingBox").removeClass("md:rounded-lg");
-    $("#navGreyLine").addClass("lg:block hidden border-b-2 b border-grey-2 w-full absolute top-[75%] left-0");
-    $("#typeOfProperty").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#ageOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#FloorArea").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#p1").addClass("hidden");
-    $("#svg1").addClass("lg:hidden");
-    $("#p2").removeClass("hidden");
-    $("#svg2").removeClass("lg:hidden");
-    $("#p3").removeClass("hidden");
-    $("#svg3").removeClass("lg:hidden");
+$(document).ready(function() {
+    // Select all tab buttons and tab content sections
+    let tabButtons = $(".adjustTabBtn");
+    let tabContents = $(".adjustTabDetail");
+  
+    // Initially hide all tab contents (No tab is active)
+    tabContents.hide();
+    tabButtons.find(".vectorBorder").hide();
+    tabButtons.find(".paratoggle").show()
+  
+    // Add click event for each tab button
+    tabButtons.on("click", function() {
+      // Remove active class from all buttons and hide all content
+      tabButtons.find(".vectorBorder").hide();
+      tabContents.hide();
+      $("#mainTabingBox").removeClass("rounded-2xl")
+      $("#mainTabingBox").addClass("rounded-t-2xl")
+      tabButtons.find(".paratoggle").show()
+  
+      // Add active class to clicked button and show related content
+      $(this).find(".vectorBorder").show();
+      $(this).find(".paratoggle").hide()
+      
+      // Get the index of the clicked tab and display the corresponding content
+      let tabIndex = tabButtons.index(this);
+      tabContents.eq(tabIndex).fadeIn(); // Using slideDown for a smooth transition
+      
+    });
 });
-
-// $(".propertySelection").click(()=>{
-//     $(".propertySelection")
-// })
-// ! Type of Property Detail Section End
-// ! Age of Property Detail Section Start
-$("#ageOfProperty").click(() => {
-    $("#ageOfProperty").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#FloorArea").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#TypePropertyDetailSection").hide();
-    $("#FloorAreaDetailSection").hide();
-    $("#AgePropertyDetailSection").fadeIn();
-    $("#p2").addClass("hidden");
-    $("#svg2").addClass("lg:hidden");
-    $("#p1").removeClass("hidden");
-    $("#svg1").removeClass("lg:hidden");
-    $("#p3").removeClass("hidden");
-    $("#svg3").removeClass("lg:hidden");
-});
-// ! Age of Property Detail Section End
-// ! Floor Area Detail Section Start
-$("#FloorArea").click(() => {
-    $("#FloorArea").addClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#typeOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#ageOfProperty").removeClass("before:w-full before:bg-turquoise md:before:block before:hidden before:absolute before:left-0 before:top-[103%] before:p-0.5");
-    $("#TypePropertyDetailSection").hide();
-    $("#AgePropertyDetailSection").hide();
-    $("#FloorAreaDetailSection").fadeIn();
-    $("#p2").removeClass("hidden");
-    $("#svg2").removeClass("lg:hidden");
-    $("#p1").removeClass("hidden");
-    $("#svg1").removeClass("lg:hidden");
-    $("#p3").addClass("hidden");
-    $("#svg3").addClass("lg:hidden");
-});
-// ! Floor Area Detail Section End
-
 
 // ! adjust.html End
 
